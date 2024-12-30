4d49c358583cb3bfc74d2fa53452647eff8714d0
Aayush Anzi <anziaayush@gmail.com>
2024-12-30 18:35:32 +0100

    Create music_library.py
    for opening music directly with command

---
+++ music_library.py
@@ -0,0 +1,6 @@
+# Define a dictionary of songs with their URLs
+music = {
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "blinding lights": "https://www.youtube.com/watch?v=fHI8X4OXluQ",
+    "levitating": "https://www.youtube.com/watch?v=TUVcZfQe-Kw",
+}
